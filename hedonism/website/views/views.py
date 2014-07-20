b4915e8ddcc9275b3fd387349cc66c070fd08ceb
Daesun Yim <daesun@robinhood.com>
2014-07-20 11:05:08 -0700

    Created separated html file for index

--- hedonism/website/views/views.py
+++ hedonism/website/views/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, Will. You're at the police station.")
+    return render(request, 'website/index.html')
